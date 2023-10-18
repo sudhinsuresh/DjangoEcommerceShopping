f1ebe2cb1fecd4fbd2b16f5a7045c995730627c8
sudhinsuresh <sudhinsureh9526@gmail.com>
2023-10-18 23:36:29 +0530

    change on couple

--- shopping/shopping/settings.py
+++ shopping/shopping/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from decouple import config
+
 from django.contrib.messages import constants as messages
 from pathlib import Path
 import os 
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-08&ekqj()sln0ix0gthyxhy43!zbq(ev+8d##5y28f7e$%x6a1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=True,cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = ['54.83.102.97']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -153,9 +153,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER= 'sudhinsuresh9526@gmail.com'
+EMAIL_HOST_PASSWORD= 'dlngswunpicyazyv'
+
 
